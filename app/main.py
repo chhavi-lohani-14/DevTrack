230b4c51688213194d0b6ab9a61d08d0ecac6126
chhavi-lohani-14 <lohani.chhavi@gmail.com>
2026-07-14 00:36:33 +0530

    feat: add database configuration, models, and schemas structure

--- app/main.py
+++ app/main.py
@@ -46,3 +46,31 @@ def get_task(task_id: int):
         status_code=404, 
         detail="Task not found"
         )
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskCreate):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            task[index] = Task(
+                id=task_id,
+                **updated_task.model_dump()
+            )
+            return tasks[index]
+        
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task.id == task_id:
+            deleted_task = tasks.pop(index)
+            return {"message": "Task deleted successfully", "task": deleted_task}
+    
+    raise HTTPException(
+        status_code=404,
+        detail="Task not found"
+    )
